d7b94cb7ea24cca60f4d84e036fcec68b6c01eb0
ILing82816 <jenny1005247@gmail.com>
2020-07-03 17:32:02 -0400

    add model_building

---
+++ model_building.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  3 10:47:16 2020
+
+@author: USER
+"""
+import pandas as pd
+import os
+#path
+path_prefix = 'D:/USA 2020 summer/Machine Learning/ds_airbnb_proj'
+#Depend on the data eda to take some features.
+df = pd.read_csv(os.path.join(path_prefix, 'airbnb_list_EDA.csv'))
+
+
+# choose relevant columns ["Location", "Rating","Number of review", 'price', 'Capacity',"Bed","Room","Bath","compound"]
+df_model = df[["Location", "Rating","Number of review", 'price', 'Capacity',"Bed","Room","Bath","compound"]]
+'''
+#Label Encoder
+from sklearn.preprocessing import LabelEncoder
+cat_features = ['Location', 'Room', 'Bath']
+encoder = LabelEncoder()
+# Apply the label encoder to each column
+encoded = df_model[cat_features].apply(encoder.fit_transform)
+df_dum = df_model[["Rating","Number of review", 'price', 'Capacity',"Bed","compound"]].join(encoded)
+'''
+
+'''
+#Count Encoder
+import category_encoders as ce
+cat_features =  ['Location', 'Room', 'Bath']
+# Create the encoder
+count_enc = ce.CountEncoder()
+# Transform the features, rename the columns with the _count suffix, and join to dataframe
+count_encoded = count_enc.fit_transform(df_model[cat_features])
+df_model = df_model.join(count_encoded.add_suffix("_count"))
+df_dum = df_model.drop(['Location', 'Room', 'Bath'], axis =1)
+'''
+
+'''
+X = df_dum.drop('price', axis =1)
+y = df_dum.price.values
+
+#Normalization
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_vars = scaler.fit_transform(X)
+
+# train test split 
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(X_vars, y, test_size=0.2)
+'''
+#Interactions
+df_model["Type_of_room"] = df_model['Room'] + "_" + df_model['Bath']
+df_model = df_model.drop(['Room', 'Bath'], axis =1)
+
+'''
+#CatBoost Encoding
+X = df_model.drop('price', axis =1)
+y = df_model.price.values
+# train test split 
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+
+import category_encoders as ce
+cat_features =  ['Location', 'Room', 'Bath']
+# Create the encoder
+target_enc = ce.CatBoostEncoder(cols=cat_features)
+target_enc.fit(X_train[cat_features], y_train)
+
+# Transform the features, rename columns with _cb suffix, and join to dataframe
+X_train = X_train.join(target_enc.transform(X_train[cat_features]).add_suffix('_cb'))
+X_train = X_train.drop(['Location', 'Room', 'Bath'], axis =1)
+X_val = X_val.join(target_enc.transform(X_val[cat_features]).add_suffix('_cb'))
+X_val = X_val.drop(['Location', 'Room', 'Bath'], axis =1)
+'''
+#CatBoost Encoding
+X = df_model.drop('price', axis =1)
+y = df_model.price.values
+# train test split 
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+
+import category_encoders as ce
+cat_features =  ['Location', 'Type_of_room']
+# Create the encoder
+target_enc = ce.CatBoostEncoder(cols=cat_features)
+target_enc.fit(X_train[cat_features], y_train)
+
+# Transform the features, rename columns with _cb suffix, and join to dataframe
+X_train = X_train.join(target_enc.transform(X_train[cat_features]).add_suffix('_cb'))
+X_train = X_train.drop(['Location', 'Type_of_room'], axis =1)
+X_val = X_val.join(target_enc.transform(X_val[cat_features]).add_suffix('_cb'))
+X_val = X_val.drop(['Location', 'Type_of_room'], axis =1)
+
+
+
+#Multiple linear regression (use mean absolute error to evaluation)
+## sklearn Linear Regression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import cross_val_score
+import numpy as np
+
+linear = LinearRegression()
+model_linear = linear.fit(X_train, y_train)
+print("Linear regression")
+print(np.mean(cross_val_score(model_linear,X_train,y_train, scoring = 'neg_mean_absolute_error', cv= 3)))
+print(np.mean(cross_val_score(model_linear,X_val,y_val, scoring = 'neg_mean_absolute_error', cv= 3)))
+
+y_train_pred = model_linear.predict(X_train)
+print(mean_absolute_error(y_train, y_train_pred))
+
+y_val_pred = model_linear.predict(X_val)
+print(mean_absolute_error(y_val, y_val_pred))
+
+# random forest regressor
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor()
+model_rf = rf.fit(X_train, y_train)
+print("Random Forest")
+print(np.mean(cross_val_score(rf,X_train,y_train,scoring = 'neg_mean_absolute_error', cv= 3)))
+print(np.mean(cross_val_score(rf,X_val,y_val,scoring = 'neg_mean_absolute_error', cv= 3)))
+
+y_train_pred = model_rf.predict(X_train)
+print(mean_absolute_error(y_train, y_train_pred))
+y_val_pred = model_rf.predict(X_val)
+print(mean_absolute_error(y_val, y_val_pred))
+
+#XGBoost 
+import xgboost as xgb
+dtrain = xgb.DMatrix(data = X_train, label = y_train)
+dval = xgb.DMatrix(data = X_val, label = y_val)
+
+param_r = {'booster' : 'gbtree'
+           #,'lambda' = ???
+           #,'alpha' = ???
+           ,'feature_selector' : 'cyclic' #also have 'shuffle', 'random', 'greedy', 'thrifty'
+           #, 'top_k' : ??? # only available for greedy and thrifty selector
+           , 'objective' : 'reg:squarederror' #also have 'squaredlogerror'
+           , 'eval_metric' : 'mae' # also have 'rmsle',
+           , 'maximize' : 'FALSE'
+        }
+
+watchlist = [(dtrain, 'train'), (dval, 'eval')]
+num_round = 50 #This is another hyperparameter of sorts
+xgb_r = xgb.train(param_r, dtrain, num_round, watchlist, early_stopping_rounds = 10)
+
